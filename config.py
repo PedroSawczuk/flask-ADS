9623516bfa65d9252f90875f5ee5e8ea84840fe8
alunoifro <alunoifro@aqs-lab-e-21.ifro.local>
2024-08-22 21:45:26 -0400

    Documentação para os pangaré baixar o arquivo cachorro do claudinei vct to puto

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class TestConfig:
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'  # Banco de dados em memória para testes
     SQLALCHEMY_TRACK_MODIFICATIONS = False
