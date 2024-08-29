c0e45311343b8263495d1a96901a0b1afb1455a3
PedroSawczuk <pedrosawczuk53@gmail.com>
2024-08-29 15:15:14 -0400

    Test update product

--- tests/test_produto_integration.py
+++ tests/test_produto_integration.py
@@ -1,32 +1,61 @@
-# test_integration.py
 import pytest
 from app import create_app, db
 from models.produto_model import Produto
 
 @pytest.fixture(scope='module')
 def client():
-    app = create_app('config.TestConfig')  # Use a test config
+    app = create_app('config.TestConfig')  # Use uma configuração de teste
     app.testing = True
 
     with app.app_context():
-        db.create_all()  # Create tables
+        db.create_all()  # Cria as tabelas
         yield app.test_client()
-        db.drop_all()  # Clean up after the test
+        db.drop_all()  # Limpa após o teste
 
 def test_add_product(client):
-    # Define product details
+    # Define os dados do produto
     produto_data = {
         'descricao': 'Produto Teste',
         'preco': '19,99'
     }
 
-    # Send a POST request to add the product
+    # Envia uma solicitação POST para adicionar o produto
     response = client.post('/produtos/', data=produto_data)
     
-    # Assert that the product was added successfully
-    assert response.status_code == 302  # Redirect after POST
+    # Verifica o código de status da resposta
+    assert response.status_code == 302  # Redireciona após POST
     
-    # Follow the redirect to check if the product exists in the database
+    # Verifica se o produto foi adicionado
     produtos = Produto.get_produtos()
+    produto = next((p for p in produtos if p.descricao == 'Produto Teste'), None)
+    assert produto is not None
+    assert produto.preco == 19.99
+
+def test_update_product(client):
+    # Adiciona um produto para atualizar
+    produto_data = {
+        'descricao': 'Produto Teste',
+        'preco': '19,99'
+    }
+    response = client.post('/produtos/', data=produto_data)
+    assert response.status_code == 302  # Redireciona após POST
+
+    # Recupera o produto adicionado
+    produto = Produto.get_produtos()
+    produto = next((p for p in produto if p.descricao == 'Produto Teste'), None)
+    assert produto is not None
+
+    # Define os dados atualizados do produto
+    update_data = {
+        'descricao': 'Produto Teste Atualizado',
+        'preco': '29,99'
+    }
+
+    # Envia uma solicitação POST para atualizar o produto
+    response = client.post(f'/produtos/editar/{produto.id}', data=update_data)
+    assert response.status_code == 302  # Redireciona após POST
     
-    assert any(p.descricao == 'Produto Teste' for p in produtos)
+    # Verifica se o produto foi atualizado
+    produto_atualizado = Produto.get_produto(produto.id)
+    assert produto_atualizado.descricao == 'Produto Teste Atualizado'
+    assert produto_atualizado.preco == 29.99
